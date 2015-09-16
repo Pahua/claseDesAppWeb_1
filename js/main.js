53e9f8e9681d38f249f04edf7535ee4db16d8829
Pahua <pahua13@gmail.com>
2015-09-16 00:09:34 -0500

    CSS and JS modified, added file to img folder, page intro almost complete

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 //username validation
+var introUsername;
+
 $(document).ready(function() {
     $('#contactForm').bootstrapValidator({
         container: '#messages',
@@ -18,7 +20,48 @@ $(document).ready(function() {
         }
 
             });
+
 });
+
+//YES READY button
+$('#swapArchived').on('click', function(e) {
+    var btn = $(this),
+        icon = btn.find('.glyphicon'),
+        text = btn.find('.text'),
+        toggleClass = 'showArchived';
+
+    if (btn.hasClass(toggleClass)) {
+        btn.removeClass('btn btn-primary btn-success showArchived').addClass('btn btn-primary btn-danger');
+    icon.removeClass('glyphicon-check').addClass('glyphicon-remove');
+        text.text('No! Not yet');
+    }
+    btn.toggleClass(toggleClass);
+});
+
+
+/*
+function readText(texto){
+    introUsername = texto; 
+    document.getElementById("introUsername").innerText = introUsername; 
+    alert("no");
+    }
+*/
+
+//introUsername=document.getElementById("username").value;
+/*
+$("#btn btn-success btn-block").click(function() {
+    // whenever the START button is clicked, read the username
+    introUsername=document.getElementById("username").value;
+    alert("DUH");
+}); 
+
+
+//print username in Jumbotron
+document.getElementById("introUsername").innerText = introUsername; 
+*/
+
+
+
 
 
     $('#exampleModal').on('show.bs.modal', function (event) {
