0df5babe6b4c6f35a4f5d8975a0ab9fee5dc5f11
Pahua <pahua13@gmail.com>
2015-09-17 22:21:26 -0500

    Minor JS changes, mainly deletion of useless code

--- js/main.js
+++ js/main.js
@@ -19,20 +19,20 @@ $(document).ready(function() {
             }
         }
 
-            });
+    });
 
 });
 
 //YES READY button
 $('#swapArchived').on('click', function(e) {
     var btn = $(this),
-        icon = btn.find('.glyphicon'),
-        text = btn.find('.text'),
-        toggleClass = 'showArchived';
+    icon = btn.find('.glyphicon'),
+    text = btn.find('.text'),
+    toggleClass = 'showArchived';
 
     if (btn.hasClass(toggleClass)) {
         btn.removeClass('btn btn-primary btn-success showArchived').addClass('btn btn-primary btn-danger');
-    icon.removeClass('glyphicon-check').addClass('glyphicon-remove');
+        icon.removeClass('glyphicon-check').addClass('glyphicon-remove');
         text.text('No! Not yet');
     }
     btn.toggleClass(toggleClass);
@@ -45,7 +45,7 @@ function readText(texto){
     document.getElementById("introUsername").innerText = introUsername; 
     alert("no");
     }
-*/
+    */
 
 //introUsername=document.getElementById("username").value;
 /*
@@ -61,27 +61,13 @@ document.getElementById("introUsername").innerText = introUsername;
 */
 
 
+$(document).ready(function() {});
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+});
 
-
-
-    $('#exampleModal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget) // Button that triggered the modal
-    var recipient = button.data('whatever') // Extract info from data-* attributes
-    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-    var modal = $(this)
-    modal.find('.modal-title').text('New message to ' +recipient)
-    modal.find('.modal-body input').val(recipient)
-    })
-
-
-    $(document).ready(function() {});
-        $(function () {
-          $('[data-toggle="tooltip"]').tooltip()
-        });
-
-        $(function () {
-          $('[data-toggle="popover"]').popover()
-        })
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
 
 
